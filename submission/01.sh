77b12aee2c699edd86ce405288299cfa2eb2f8f8
lebon8 <drnlebonenian@gmail.com>
2026-06-25 01:10:16 +0200

    try 4

--- submission/01.sh
+++ submission/01.sh
@@ -1,3 +1,3 @@
 # Write the bitcoin cli command to get the bitcoin node installation version 
 
-bitcoin-cli -regtest getnetworkinfo | jq -r '.subversion' | sed 's|/Satoshi:||; s|/||'
+bitcoin-cli --version
